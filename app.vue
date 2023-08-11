729aeff860ece675c3c94d5e13d9907376fbf6dc
Maximilian Dietel <maxdietel03@gmail.com>
2023-08-11 17:05:19 +0200

    feat: Add responsiveness

--- app.vue
+++ app.vue
@@ -1,48 +1,53 @@
 <template>
-  <div v-if="showContent" class="container">
-    <ConfettiExplosion
-      :particleCount="900"
-      :force="0.7"
-      :duration="5000"
-      :shouldDestroyAfterDone="true"
-    />
-    <p class="happyBDay">Happy Birthday!!!</p>
-    <ImgSlider />
-  </div>
-  <div v-if="!showContent" class="modal">
-    <p class="happyBDay">We want to celebrate your birthday with you today!</p>
-    <button @click="hideModal()">Continue to you Party!</button>
-  </div>
+  <main class="main">
+    <h1 class="colored-heading">{{showPictures ? "Happy Birthday!!!" : "We want to celebrate your birthday with you today!" }}</h1>
+
+    <div v-if="showPictures" class="confetti-container">
+      <ConfettiExplosion
+        :particleCount="900"
+        :force="0.7"
+        :duration="5000"
+        :shouldDestroyAfterDone="true"
+      />
+    </div>
+    <ImgSlider v-if="showPictures" />
+
+    <button v-else="showPictures" @click="hideModal()" class="orange-button">Continue to your party!</button>
+  </main>
 </template>
 
 <script setup>
-// import function to register Swiper custom elements
 import ImgSlider from "@/components/ImgSlider.vue";
 import ConfettiExplosion from "vue-confetti-explosion";
 import "regenerator-runtime/runtime";
 
-function playAudio() {
-  const audio = new Audio("/sounds/happyBirthday.mp3");
-
-  audio.play();
-  console.log("playing");
-}
+const showPictures = ref(false);
 
 function hideModal() {
-  playAudio();
-  showContent.value = true;
-}
+  const audio = new Audio("/sounds/happyBirthday.mp3");
+  audio.play();
 
-const showContent = ref(false);
+  showPictures.value = true;
+}
 </script>
 
 <style scoped>
-.happyBDay {
-  font-size: 7.5rem;
+.main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  gap: 4rem;
+  padding: 4rem;
+  font-family: Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+}
+
+.colored-heading {
+  font-size: 4rem;
   text-align: center;
   font-weight: bold;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: underline;
   background-image: linear-gradient(
     50deg,
     #ca4246 16.666%,
@@ -61,43 +66,37 @@ const showContent = ref(false);
   -moz-background-clip: text;
   -webkit-text-fill-color: transparent;
   -moz-text-fill-color: transparent;
-  margin-bottom: 1rem;
-}
-.container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  transition: 1s ease-in-out;
 }
 
-.modal {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-}
-
-.modal p {
-  font-size: 3.5rem;
-}
-
-.modal button {
-  font-size: 2rem;
+.orange-button {
+  all: unset;
   padding: 1rem 2rem;
   border-radius: 5px;
   background-color: #e16541;
   color: white;
-  border: none;
   cursor: pointer;
-  margin-top: 2rem;
-  transition: all 0.3s ease-in-out;
+}
+.orange-button:hover {
+  opacity: 0.9;
+}
+
+.confetti-container {
+  position: absolute;
+  inset: auto;
+  top: 0;
+  height: 10vh;
+  z-index: 100;
+  background-color: #476098;
+}
+
+@media (max-width: 768px) {
+  .main {
+    padding: 2rem;
+    gap: 2rem;
+  }
+  .colored-heading {
+    font-size: 2rem;
+  }
 }
 </style>
 
